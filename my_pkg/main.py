1692083afcd7b92f960e9e24aeddc288bc9bc0f5
chrisoro <sloboda@gmx.de>
2023-02-04 12:56:05 +0100

    init

---
+++ my_pkg/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+def my_sum(a: int, b: int) -> int:
+    return a + b
+
+
+def my_diff(a: int, b: int) -> int:
+    return a - b
---
+++ my_pkg/tests/test_main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from ..main import my_diff, my_sum
+
+
+def test_my_sum() -> None:
+    assert my_sum(1, 1) == 2
